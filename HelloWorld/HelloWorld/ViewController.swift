13d829c14e8b4cf08f35960b7efb441c51107052
adolfo <correo.de.adolfo@gmail.com>
2015-08-01 19:02:08 -0400

    Created HelloWorld project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Adolfo Rodriguez on 2015-08-01.
+//  Copyright (c) 2015 Relevant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
