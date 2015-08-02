a288e73a2e378b920af338eb339e3c0c4f204bce
adolfo <correo.de.adolfo@gmail.com>
2015-08-01 21:22:02 -0400

    Set view controller classes

---
+++ HelloWorld/HelloWorld/MontrealViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MontrealViewController.swift
+//  HelloWorld
+//
+//  Created by Adolfo Rodriguez on 2015-08-01.
+//  Copyright (c) 2015 Relevant. All rights reserved.
+//
+
+import UIKit
+
+class MontrealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
